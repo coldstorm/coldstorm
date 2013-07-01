46d95c75a83d72e4e573973441cc72f01cfe603b
Kevin Brown <bass5098@gmail.com>
2013-06-30 21:45:26 -0400

    Working on improving the CSS and structure

--- scripts/controllers/tabs.js
+++ scripts/controllers/tabs.js
@@ -2,7 +2,6 @@ Coldstorm.controller("TabsCtrl", ["$scope", function($scope)
 {
     $scope.channelEquals = function(first, second)
     {
-        console.log(first, second, first.name == second.name, first == second);
         return first.name == second.name;
     };
 }]);
