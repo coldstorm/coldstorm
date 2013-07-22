9efc87398fe5398dcdaf1a0cc41d622d8793798c
Express50 <press.ex50@gmail.com>
2013-07-22 19:36:29 -0400

    Started working on user options pop-up

---
+++ scripts/controllers/useroptions.js
@@ -0,0 +1,4 @@
+Coldstorm.controller("UserOptionsCtrl", ["$scope", function($scope)
+{
+    $scope.userOptions = true;
+}]);
---
+++ scripts/directives/useroptions.js
@@ -0,0 +1,11 @@
+Coldstorm.directive("useroptions", function()
+{
+    return {
+        controller: "UserOptionsCtrl",
+        replace: true,
+        restrict: "E",
+        scope: { user: "=user" },
+        templateUrl: "views/useroptions.html",
+        transclude: true
+    };
+});
