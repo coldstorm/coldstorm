51ff0ee903c68ce9865fd7bdcf3f1a8ee7b57a73
Kevin Brown <bass5098@gmail.com>
2013-08-09 21:02:24 -0400

    Started work on the views for queries
    Pretty much copied the Channel for now, still need to build out
    a lot of it.

---
+++ scripts/controllers/query.js
@@ -0,0 +1,65 @@
+Controllers.controller("QueryCtrl",
+    ["$scope", "$routeParams", "$location", "User", "Channel", "Connection",
+    function ($scope, $routeParams, $location, User, Channel, Connection)
+    {
+        $scope.user = User.get("~");
+
+        if ($scope.user.nickName === "")
+        {
+            $location.path("/login");
+            return;
+        }
+
+        var nickName = $routeParams.nickName
+
+        $scope.query = Query.get(nickName);
+
+        $scope.send = function ()
+        {
+            var line = $scope.input.text;
+
+            if (line.length < 1)
+            {
+                return;
+            }
+
+            // Clear the line
+
+            $scope.input.text = "";
+
+            if (line[0] === "/")
+            {
+                Connection.send(line.substring(1))
+
+                return;
+            }
+            
+            $scope.channel.addLine(line, $scope.user);
+
+            Connection.send("PRIVMSG " + $scope.query.user.nickName + " " +
+                            line);
+        };
+
+        $scope.$watch("query.active", function ()
+        {
+            $scope.query.active = false;
+        });
+
+        $scope.$on("query.close", function (evt, query)
+        {
+            if (query == $scope.query)
+            {
+                var channels = $scope.channels;
+
+                if (channels.length > 1)
+                {
+                    $location.path("/channels/" + channels[0].name);
+                }
+            }
+        });
+
+        $scope.$watch(function ()
+        {
+            $scope.channels = Channel.all();
+        });
+    }]);
