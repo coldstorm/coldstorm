999219856736a0e05f472022e9b962696a2f9ad5
Kevin Brown <bass5098@gmail.com>
2013-08-11 14:50:14 -0400

    No need for the directive

--- scripts/filters/spoiler.js
+++ scripts/filters/spoiler.js
@@ -1,10 +1,10 @@
-Filters.filter("spoiler", ["$filter", function ($filter)
+Filters.filter("spoiler", ["$compile", function ($compile)
 {
     return function (input)
     {
         var line = input;
 
-        line = line.replace(/::(.+?)::/, "<spoiler>$1</spoiler>");
+        line = line.replace(/::(.+?)::/g, '<span class="spoiler">$1</span>');
 
         return line;
     };
