132fc1e35c783cfbed7f9ca5e4871b5df7a4a270
Kevin Brown <bass5098@gmail.com>
2013-08-11 15:35:21 -0400

    Fixed quote/ampersand issue

--- scripts/filters/line.js
+++ scripts/filters/line.js
@@ -2,11 +2,7 @@ Filters.filter("line", ["$filter", function ($filter)
 {
     return function (input)
     {
-        var line = input.replace(/&/g, '&amp;').
-                         replace(/</g, '&lt;').
-                         replace(/>/g, '&gt;').
-                         replace(/'/g, '&#39;').
-                         replace(/"/g, '&quot;');
+        var line = input;
 
         line = $filter("linky")(line);
         line = $filter("spoiler")(line);
