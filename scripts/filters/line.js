1b08f6e834656c7ebc540747b0d43df41d4163a0
Kevin Brown <bass5098@gmail.com>
2013-08-11 15:46:18 -0400

    Links open in new tabs

--- scripts/filters/line.js
+++ scripts/filters/line.js
@@ -5,6 +5,9 @@ Filters.filter("line", ["$filter", function ($filter)
         var line = input;
 
         line = $filter("linky")(line);
+
+        line = line.replace("<a href", '<a target="_blank" href');
+
         line = $filter("spoiler")(line);
         line = $filter("highlight")(line);
 
