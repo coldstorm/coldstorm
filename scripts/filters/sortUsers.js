102690b199d9fa1614a7c6e7175bdbec85edbc78
Kevin Brown <bass5098@gmail.com>
2013-08-11 18:31:16 -0400

    Added sortUsers filter for user list

---
+++ scripts/filters/sortUsers.js
@@ -0,0 +1,22 @@
+Filters.filter("sortUsers", [function () {
+    return function (input, channel) {
+        var users = input.sort(function (a, b)
+        {
+            var ranks = ["", "+", "%", "@"];
+
+            if (a.ranks[channel.name] != b.ranks[channel.name])
+            {
+                if (ranks.indexOf(a.ranks[channel.name]) > ranks.indexOf(b.ranks[channel.name]))
+                {
+                    return -1;
+                }
+
+                return 1;
+            }
+
+            return a.nickName.localeCompare(b.nickName);
+        });
+
+        return users;
+    };
+}]);
