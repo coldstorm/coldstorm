974ac770f9174b2f327f91fda3a8ccee1fe87273
Kevin Brown <bass5098@gmail.com>
2013-08-11 20:58:13 -0400

    Finished fg color support

--- scripts/filters/color.js
+++ scripts/filters/color.js
@@ -3,7 +3,7 @@ Filters.filter("color", [function ()
     return function (input)
     {
         var line = input.replace(/\\c/gi, "\u0003");
-        line = line.replace(/\u0003([0-9]{1,2})(.+)/g,
+        line = line.replace(/\u0003([0-9]{1,2})([^\n\u000F\u0003]*)/g,
             '<span class="c$1">$2</span>');
 
         return line;
