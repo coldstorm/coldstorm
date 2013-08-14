0a51adbaa71017efcb37bbf941ee812f8083eb92
Kaslai <aslaii@hotmail.com>
2013-08-14 08:04:48 -0700

    Made the highlight regex more sane... I think?

--- scripts/filters/highlight.js
+++ scripts/filters/highlight.js
@@ -3,21 +3,7 @@ Filters.filter("highlight", ["User", function (User)
     return function (input)
     {
         var user = User.get("~");
-        //(\ test\ )|(^test\ +)|(^\ ?test\ ?$)|(\ test\ ?$)
-        var re = new RegExp("(\ " + user.nickName +
-            "\ )|(^" + user.nickName +
-            "\ +)|(^\ ?" + user.nickName +
-            "\ ?$)|(\ " + user.nickName + 
-            "\ ?$)", "ig");
-        var matches = input.match(re);
-
-        if (matches != null)
-        {
-            var line = input.replace(matches[0], '<span class="highlight">' +
-                matches[0] + '</span>');
-
-            return line;
-        }
-        return input;
+        var re = new RegExp( "([^\\w\\d]|^)(" + user.nickName + ")([^\\w\\d]|$)", "ig" );
+	return input.replace( re, '$1<span class="highlight">$2</span>$3' );
     };
 }]);
