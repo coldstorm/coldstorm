e7f2ad0d8b8142671e685398154943c22d391a23
Express50 <press.ex50@gmail.com>
2013-08-14 09:49:33 -0400

    Fix "Hitting login twice causes undefined behavior" (https://trello.com/c/aQc6OaJT/44-hitting-login-twice-causes-undefined-behavior)

--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -42,82 +42,88 @@ Controllers.controller("LoginCtrl",
             $scope.openModal();
         });
 
+        $scope.connecting = false;
+
         $scope.login = function ()
         {
-            var hostToken = "";
-
-            $http.jsonp("http://kaslai.us/coldstorm/fixip.php?nick=" +
-                encodeURI($scope.user.nickName) + "&random=" +
-                Math.floor(Math.random() * 10000000));
-
-            $cookies.nickName = $scope.user.nickName;
-            $cookies.color = $scope.user.color;
-
-            hostToken = md5($scope.user.nickName);
-
-            if (VERSION == "local")
+            if ($scope.connecting === false)
             {
-                Connection.connect("ws://coldstorm.tk:82");
-            } else
-            {
-                Connection.connect("ws://coldstorm.tk:81");
-            }
+                $scope.connecting = true;
+                var hostToken = "";
 
-            Connection.onOpen(function ()
-            {
-                Connection.send("NICK " + $scope.user.nickName);
-                Connection.send("USER " +
-                    $scope.user.color.substring(1).toUpperCase() +
-                    $scope.user.flag + " - - :New coldstormer");
+                $http.jsonp("http://kaslai.us/coldstorm/fixip.php?nick=" +
+                    encodeURI($scope.user.nickName) + "&random=" +
+                    Math.floor(Math.random() * 10000000));
 
-                Connection.onWelcome(function ()
+                $cookies.nickName = $scope.user.nickName;
+                $cookies.color = $scope.user.color;
+
+                hostToken = md5($scope.user.nickName);
+
+                if (VERSION == "local")
                 {
-                    if ($scope.user.password)
-                    {
-                        Connection.send("PRIVMSG NickServ :identify " +
-                            $scope.user.password);
-                    }
-
-                    if (hostToken)
-                    {
-                        Connection.send("PRIVMSG Jessica :~fixmyip " +
-                            hostToken);
-                    }
-
-                    $rootScope.requestNotifications();
-                });
-            });
-
-            Connection.onMessage(function (message)
-            {
-                if (message.indexOf("NOTICE " + $scope.user.nickName +
-                    " :Tada") > -1)
+                    Connection.connect("ws://coldstorm.tk:82");
+                } else
                 {
-                    if (VERSION == "local")
-                    {
-                        var test = Channel.register("#test");
-
-                        test.join();
-
-                        $location.path("/channels/#test");
-                    } else
-                    {
-                        var cs = Channel.register("#Coldstorm");
-                        var two = Channel.register("#2");
-
-                        cs.join();
-                        two.join();
-
-                        $location.path("/channels/#Coldstorm");
-                    }
+                    Connection.connect("ws://coldstorm.tk:81");
                 }
 
-                Parser.parse(message);
-            });
+                Connection.onOpen(function ()
+                {
+                    Connection.send("NICK " + $scope.user.nickName);
+                    Connection.send("USER " +
+                        $scope.user.color.substring(1).toUpperCase() +
+                        $scope.user.flag + " - - :New coldstormer");
 
-            Connection.onClose(function ()
-            {
-                console.log("Closed");
-            });
+                    Connection.onWelcome(function ()
+                    {
+                        if ($scope.user.password)
+                        {
+                            Connection.send("PRIVMSG NickServ :identify " +
+                                $scope.user.password);
+                        }
+
+                        if (hostToken)
+                        {
+                            Connection.send("PRIVMSG Jessica :~fixmyip " +
+                                hostToken);
+                        }
+
+                        $rootScope.requestNotifications();
+                    });
+                });
+
+                Connection.onMessage(function (message)
+                {
+                    if (message.indexOf("NOTICE " + $scope.user.nickName +
+                        " :Tada") > -1)
+                    {
+                        if (VERSION == "local")
+                        {
+                            var test = Channel.register("#test");
+
+                            test.join();
+
+                            $location.path("/channels/#test");
+                        } else
+                        {
+                            var cs = Channel.register("#Coldstorm");
+                            var two = Channel.register("#2");
+
+                            cs.join();
+                            two.join();
+
+                            $location.path("/channels/#Coldstorm");
+                        }
+                    }
+
+                    Parser.parse(message);
+                });
+
+                Connection.onClose(function ()
+                {
+                    console.log("Closed");
+                });
+            }
         };
     }]);
