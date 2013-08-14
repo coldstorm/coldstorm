ea1018adb7118f27155dbd92fae04ac59193e99d
Express50 <press.ex50@gmail.com>
2013-08-14 17:31:48 -0400

    Fix bug where a notification will be displayed when someone else pings themselves (https://trello.com/c/tN7ruyzf/58-sometimes-the-window-title-displays-a-notification-when-there-isn-t-one)

--- scripts/services/channel.js
+++ scripts/services/channel.js
@@ -16,7 +16,7 @@ Services.factory("Channel", ["$rootScope", "User", "Notifications", function ($r
         channel.addLine(line, user);
 
         var myUser = User.get("~");
-        var re = new RegExp( "([^\\w\\d]|^)(" + user.nickName + ")(\\s?" + user.nickName + ")*([^\\w\\d]|$)", "ig" );
+        var re = new RegExp("([^\\w\\d]|^)(" + myUser.nickName + ")(\\s?" + myUser.nickName + ")*([^\\w\\d]|$)", "ig");
         var matches = message.line.match(re);
 
         if ($rootScope.blurred)
