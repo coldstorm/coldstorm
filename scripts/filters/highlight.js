39081c946695f9874291b4aa43a567fce5d0d9f0
Express50 <press.ex50@gmail.com>
2013-08-14 08:37:19 -0400

    Fix highlight rule is too general

--- scripts/filters/highlight.js
+++ scripts/filters/highlight.js
@@ -3,9 +3,21 @@ Filters.filter("highlight", ["User", function (User)
     return function (input)
     {
         var user = User.get("~");
-        var line = input.replace(user.nickName, '<span class="highlight">' +
-            user.nickName + '</span>');
+        //(\ test\ )|(^test\ +)|(^\ ?test\ ?$)|(\ test\ ?$)
+        var re = new RegExp("(\ " + user.nickName +
+            "\ )|(^" + user.nickName +
+            "\ +)|(^\ ?" + user.nickName +
+            "\ ?$)|(\ " + user.nickName + 
+            "\ ?$)", "ig");
+        var matches = input.match(re);
 
-        return line;
+        if (matches != null)
+        {
+            var line = input.replace(matches[0], '<span class="highlight">' +
+                matches[0] + '</span>');
+
+            return line;
+        }
+        return input;
     };
 }]);
