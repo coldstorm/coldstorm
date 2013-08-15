77c3f744e6ae46801bc1a7cddd9b08a4e893c113
Express50 <press.ex50@gmail.com>
2013-08-15 17:14:40 -0400

    Fix motd links open in current tab

---
+++ scripts/filters/motd.js
@@ -0,0 +1,15 @@
+﻿Filters.filter("motd", ["$filter", function ($filter)
+{
+    return function (input)
+    {
+        var motd = input;
+
+        motd = $filter("linky")(line);
+
+        motd = line.replace("<a href", '<a target="_blank" href');
+
+        motd = $filter("color")(line);
+
+        return line;
+    };
+}]);
