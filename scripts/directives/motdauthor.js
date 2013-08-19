043600f9dbc5eae5957a2d5d27a6eb919fa8bd4f
Express50 <press.ex50@gmail.com>
2013-08-19 10:39:58 -0400

    Add motdauthor directive

---
+++ scripts/directives/motdauthor.js
@@ -0,0 +1,11 @@
+﻿Directives.directive("motdauthor", function ()
+{
+    return {
+        controller: "MotdAuthorCtrl",
+        replace: true,
+        restrict: "E",
+        scope: { user: "=user" },
+        templateUrl: "views/motdauthor.html",
+        transclude: true
+    }
+});
