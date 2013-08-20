cf95b4fd5e5da2a4dd38ed720cc6e67d0d57da04
Kevin Brown <bass5098@gmail.com>
2013-08-20 19:04:50 -0400

    Fixed the bold/italics/underline to not be as greedy

--- scripts/filters/bold.js
+++ scripts/filters/bold.js
@@ -6,7 +6,8 @@
         {
 
             var line = input.replace(/&#2;/g, "\u0002");
-            line = line.replace(/[\u0002]([^\n\u000F\u0002]*)[\u0002]/g,
+
+            line = line.replace(/\u0002(.*?)[\n\u000F\u0002]/g,
                 '<span class="bold">$1</span>');
 
             return line;
--- scripts/filters/underline.js
+++ scripts/filters/underline.js
@@ -6,7 +6,7 @@
         {
 
             var line = input.replace(/&#31;/g, "\u001F");
-            line = line.replace(/[\u001F]([^\n\u000F\u001F]*)[\u001F]/g,
+            line = line.replace(/\u001F(.*?)[\n\u000F\u001F]/g,
                 '<span class="underlined">$1</span>');
 
             return line;
