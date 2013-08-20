c18ac88b0b9da4d737056d15113f589e50b2c243
Express50 <press.ex50@gmail.com>
2013-08-20 15:05:39 -0400

    Add Input service

---
+++ scripts/services/input.js
@@ -0,0 +1,15 @@
+﻿Services.factory("Input", ["$rootScope", "Connection", function ($rootScope, Connection)
+{
+    $rootScope.process = function (input)
+    {
+        //Parse commands
+
+        //Parse formatting
+
+        //Send the line
+
+        //Clear the input
+
+        //Add the line to the current tab
+    }
+}]);
