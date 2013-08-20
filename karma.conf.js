78290418f9f0e4b48aa509c37a60781ee9959d87
Express50 <press.ex50@gmail.com>
2013-08-20 08:21:07 -0400

    Rename "test" to "tests" in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'scripts/**/*.js',
-      'test/unit/**/*.js'
+      'tests/unit/**/*.js'
     ],
 
 
