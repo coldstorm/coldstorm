8d14315c5f2d153b63a47f80dd46355f6a36b360
Express50 <press.ex50@gmail.com>
2013-08-20 18:22:31 -0400

    Fix regexes to support turning off formatting

--- scripts/filters/italics.js
+++ scripts/filters/italics.js
@@ -5,7 +5,7 @@
         if (input)
         {
             var line = input.replace(/&#29;/g, "\u001D");
-            line = line.replace(/\u001D([^\n\u000F\u001D]*)/g,
+            line = line.replace(/[\u001D]([^\n\u000F\u001D]*)[\u001D]/g,
                 '<span class="italics">$1</span>');
 
             return line;
