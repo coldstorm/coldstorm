a245b9597ed0e3b1cc21478fd6df2e323025d74f
Express50 <press.ex50@gmail.com>
2013-08-21 08:40:10 -0400

    Add message sending using Input service

--- scripts/services/input.js
+++ scripts/services/input.js
@@ -1,15 +1,25 @@
-﻿Services.factory("Input", ["$rootScope", "Connection", function ($rootScope, Connection)
+﻿Services.factory("Input", ["$rootScope", "Connection", "User",  function ($rootScope, Connection, User)
 {
-    $rootScope.process = function (input)
+    $rootScope.process = function (input, target)
     {
+        var line = input.text;
         //Parse commands
 
         //Parse formatting
 
         //Send the line
+        if (target.name) //it's a channel
+        {
+            Connection.send("PRIVMSG " + target.name + " :" + line);
+        } else if (target.user) //it's a query
+        {
+            Connection.send("PRIVMSG " + target.user.nickName + " :" + line);
+        }
 
         //Clear the input
+        input.text = "";
 
         //Add the line to the current tab
+        target.addLine(line, User.get("~"));
     }
 }]);
