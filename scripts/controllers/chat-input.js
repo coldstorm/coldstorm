85a134bfdddaecd9f5cd34044eb636c1e92d088c
Express50 <press.ex50@gmail.com>
2013-08-21 12:00:06 -0400

    Add chatInput directive

---
+++ scripts/controllers/chat-input.js
@@ -0,0 +1,7 @@
+﻿Controllers.controller("ChatInputCtrl", ["$scope", "$rootScope", function ($scope, $rootScope)
+{
+    $scope.submit = function ()
+    {
+        $rootScope.process($scope.input, $scope.tab);
+    }
+}]);
---
+++ scripts/directives/chat-input.js
@@ -0,0 +1,11 @@
+﻿Directives.directive("chatInput", function ()
+{
+    return {
+        controller: "ChatInputCtrl",
+        replace: true,
+        restrict: "E",
+        scope: { input: "=input", tab: "=tab" },
+        templateUrl: "views/chat-input.html",
+        transclude: true
+    }
+});
