8c59a1e9143704ff5c3d14088d030aca971432b1
Express50 <press.ex50@gmail.com>
2013-09-02 12:28:29 -0400

    Add PhantomJS to browsers and set singleRun to true

--- karma.conf.js
+++ karma.conf.js
@@ -59,7 +59,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'PhantomJS'],
 
 
     // If browser does not capture in given timeout [ms], kill it
@@ -68,6 +68,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: true
   });
 };
