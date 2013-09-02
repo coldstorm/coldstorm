f1f0fea16a291ee2754ff1ae4408559f3fd9abda
Express50 <press.ex50@gmail.com>
2013-09-02 16:57:41 -0400

    Add kick and ban functionality to useroptions

--- scripts/controllers/useroptions.js
+++ scripts/controllers/useroptions.js
@@ -1,9 +1,19 @@
-Controllers.controller("UserOptionsCtrl", ["$scope", "$rootScope", "$location", "Query",
-function ($scope, $rootScope, $location, Query)
+Controllers.controller("UserOptionsCtrl", ["$scope", "$rootScope", "$location", "Query", "Connection",
+function ($scope, $rootScope, $location, Query, Connection)
 {
     $scope.query = function (user)
     {
         var query = Query.register(user.nickName);
         $location.path("/query/" + query.name);
+    };
+
+    $scope.kick = function (user)
+    {
+        Connection.send("KICK " + $scope.channel.name + " " + user.nickName);
+    };
+
+    $scope.ban = function (user)
+    {
+        Connection.send("MODE " + $scope.channel.name + " +b " + user.nickName);
     };
 }]);
--- scripts/directives/useroptions.js
+++ scripts/directives/useroptions.js
@@ -4,7 +4,7 @@ Directives.directive("useroptions", function ()
         controller: "UserOptionsCtrl",
         replace: true,
         restrict: "E",
-        scope: { user: "=user" },
+        scope: { user: "=user", channel: "=channel" },
         templateUrl: "views/useroptions.html",
         transclude: true
     };
