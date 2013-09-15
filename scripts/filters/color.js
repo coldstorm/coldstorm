7379d8c9712fcfe4092159012ca363d3126bd2d0
Yonom <sepehr1376@gmail.com>
2013-09-15 18:08:13 +0200

    Implement mIRC background color support

--- scripts/filters/color.js
+++ scripts/filters/color.js
@@ -6,6 +6,11 @@ Filters.filter("color", [function ()
         {
             var line = input.replace(/\\c/gi, "\u0003");
             line = line.replace(/&#3;/g, "\u0003");
+
+            line = line.replace(/\u0003([0-9]{1,2}),([0-9]{1,2})([^\n\u000F\u0003]*)/g,
+                '<span class="c$1 bc$2">$3</span>');
+            line = line.replace(/\u0003,([0-9]{1,2})([^\n\u000F\u0003]*)/g,
+                '<span class="bc$1">$2</span>');
             line = line.replace(/\u0003([0-9]{1,2})([^\n\u000F\u0003]*)/g,
                 '<span class="c$1">$2</span>');
 
