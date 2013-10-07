68389dd8cc07f6773c55cd6c1457f9f40313a3da
Kaslai <aslaii@hotmail.com>
2013-10-07 15:37:38 -0700

    Higlight regex even less stupid now

--- scripts/filters/highlight.js
+++ scripts/filters/highlight.js
@@ -3,7 +3,7 @@ Filters.filter("highlight", ["User", function (User)
     return function (input)
     {
         var user = User.get("~");
-        var re = new RegExp( "([^\\w\\d]|^)(" + user.nickName + ")((\\s?" + user.nickName + ")*)([^\\w\\d]|$)", "ig" );
-	return input.replace( re, '$1<span class="highlight">$2$3</span>$5' );
+        var re = new RegExp( "\\b(" + user.nickName + ")\\b", "ig" );
+	return input.replace( re, '<span class="highlight">$1</span>' );
     };
 }]);
