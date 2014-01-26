4848d828a20c5f8f76907858c6314e81e4393b18
Express50 <press.ex50@gmail.com>
2014-01-26 18:15:41 -0500

    Add default backlog amount of 250

--- app/scripts/controllers/lines.js
+++ app/scripts/controllers/lines.js
@@ -1,7 +1,12 @@
 Controllers.controller("LinesCtrl", ["$scope", "$rootScope", function ($scope, $rootScope)
 {
+    $scope.limit = 250;
+
     $rootScope.$watch(function (scope)
     {
-        $scope.limit = scope.settings.BACKLOG_AMOUNT;
+        if (scope.settings.BACKLOG_AMOUNT)
+        {
+            $scope.limit = scope.settings.BACKLOG_AMOUNT;
+        }
     });
 }]);
