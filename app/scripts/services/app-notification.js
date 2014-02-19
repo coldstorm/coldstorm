f2034ad3295c9284cf9da47d872deb378c2e3ea8
Express50 <press.ex50@gmail.com>
2014-02-18 21:50:16 -0500

    Implement fetchUpdate()
    Use GitHub Events API to find the most recent PushEvent to the branch gh-pages. Then check the SHA of the commit against $rootScope.meta.version. If the hashes differ, set the appNotification.

--- app/scripts/services/app-notification.js
+++ app/scripts/services/app-notification.js
@@ -1,8 +1,51 @@
 Services.factory("AppNotification",
-["$rootScope", 
-function ($rootScope)
+["$rootScope", "$http", "$timeout",
+function ($rootScope, $http, $timeout)
 {
-    $rootScope.appNotification = "This is a test notification.";
+    $rootScope.appNotification = "";
+
+    var timeout = 60 * 1000;
+
+    var fetchUpdate = function ()
+    {
+        $timeout(function ()
+        {
+            $http.jsonp("https://api.github.com/repos/coldstorm/coldstorm/events?callback=JSON_CALLBACK")
+            .success(function (data, status, headers)
+            {
+                if (status == 200)
+                {
+                    var events = data["data"];
+                    var commit;
+
+                    for (var i = 0; i < events.length; i++) 
+                    {
+                        if (events[i].type == "PushEvent" && events[i].payload.ref == "refs/heads/gh-pages")
+                        {
+                            commit = events[i].payload.commits[0];
+                            break;
+                        }
+                    };
+
+                    if (commit)
+                    {
+                        if (commit.sha != $rootScope.meta.version)
+                        {
+                            $rootScope.appNotification = "Reload the page to get the newest version of Coldstorm!";
+                        }
+                    }
+                }
+            })
+            .error(function (data, status, headers)
+            {
+                
+            });
+
+            fetchUpdate();
+        }, timeout)
+    };
+
+    fetchUpdate();
 
     return {
         dismiss: function ()
